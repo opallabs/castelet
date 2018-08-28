2b254819a4c96c545c53aaa6342c55d5aa0a855d
John Bragg <remotezygote@gmail.com>
2018-08-28 09:47:34 -0700

    fix lint issues

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ if (process.env.NODE_ENV === 'test') {
   debug('using test launcher (mock)')
   launcher = () => ({
     launched: true,
-    close: async () => { return true },
-    pages: async () => { return [] },
+    close: async () => true,
+    pages: async () => [],
   })
 }
 

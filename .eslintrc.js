ef17e340f04f92cba742dcca132b7635ab704009
John Bragg <remotezygote@gmail.com>
2018-08-28 09:12:58 -0700

    adds linter

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: 'airbnb-base',
+  rules: {
+    semi: [
+      'error',
+      'never'
+    ],
+    'arrow-parens': [
+      'error',
+      'as-needed'
+    ],
+    'eol-last': [
+      'error',
+      'never'
+    ]
+  }
+};
